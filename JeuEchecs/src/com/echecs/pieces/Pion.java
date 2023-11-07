4fc7dd5d48616e450037f70941fed5c6de1dc817
GuiFlam <gui.flamand@gmail.com>
2023-11-06 22:58:29 -0500

    Pion

--- JeuEchecs/src/com/echecs/pieces/Pion.java
+++ JeuEchecs/src/com/echecs/pieces/Pion.java
@@ -7,9 +7,30 @@ public class Pion extends Piece
     public Pion(char couleur) {
         super(couleur);
     }
-
     @Override
     public boolean peutSeDeplacer(Position pos1, Position pos2, Piece[][] echiquier) {
+        if(Character.compare(couleur, 'b') == 0 && pos1.getLigne() == 1 && pos2.getLigne() - pos1.getLigne() <= 2) {
+            return true;
+        }
+        else if(Character.compare(couleur, 'n') == 0 && pos1.getLigne() == 6 && pos1.getLigne() - pos2.getLigne() <= 2) {
+            return true;
+        }
+        else if(Character.compare(couleur, 'b') == 0 && pos2.getLigne() - pos1.getLigne() == 1    && Character.compare(pos1.getColonne(), pos2.getColonne()) == 0) {
+            return true;
+        }
+        else if(Character.compare(couleur, 'n') == 0 && pos1.getLigne() - pos2.getLigne() == 1    && Character.compare(pos1.getColonne(), pos2.getColonne()) == 0) {
+            return true;
+        }
+        else if(Character.compare(couleur, 'b') == 0    && 
+        Math.abs((int)pos1.getColonne() - (int)pos2.getColonne()) == 1  && 
+        pos2.getLigne() - pos1.getLigne() == 1) {
+            return true;
+        }
+        else if(Character.compare(couleur, 'n') == 0    && 
+                Math.abs((int)pos1.getColonne() - (int)pos2.getColonne()) == 1  && 
+                pos1.getLigne() - pos2.getLigne() == 1) {
+            return true;
+        }
         return false;
     }
 }
